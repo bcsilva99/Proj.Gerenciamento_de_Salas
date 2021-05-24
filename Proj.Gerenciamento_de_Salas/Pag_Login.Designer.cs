202ab7277805c3da7fa2dba27497fb796a3a5dc1
Bruna Caroline Silva <brunacsilva11@outlook.com>
2021-05-24 20:53:21 -0300

    Teste final da noite

--- Proj.Gerenciamento_de_Salas/Pag_Login.Designer.cs
+++ Proj.Gerenciamento_de_Salas/Pag_Login.Designer.cs
@@ -31,6 +31,7 @@ namespace Proj.Gerenciamento_de_Salas
         {
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // textBox2
@@ -49,11 +50,19 @@ namespace Proj.Gerenciamento_de_Salas
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(248, 114);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(252, 20);
+            this.textBox1.TabIndex = 3;
+            // 
             // Pag_Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.MaximizeBox = false;
@@ -69,6 +78,7 @@ namespace Proj.Gerenciamento_de_Salas
         #endregion
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
