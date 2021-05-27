8c674f1c5c5c0a1219fdb7ba99d471bc9165cd58
Bruna Caroline Silva <brunacsilva11@outlook.com>
2021-05-27 08:29:53 -0300

    Teste Merge 2

--- Proj.Gerenciamento_de_Salas/Pag_CadastroUsuario.Designer.cs
+++ Proj.Gerenciamento_de_Salas/Pag_CadastroUsuario.Designer.cs
@@ -29,12 +29,31 @@ namespace Proj.Gerenciamento_de_Salas
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(88, 85);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(370, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Pag_CadastroUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Pag_CadastroUsuario";
             this.Text = "Pag_CadastroUsuario";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
