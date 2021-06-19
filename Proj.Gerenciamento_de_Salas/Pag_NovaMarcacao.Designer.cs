6c5034f1691e03773aad73f5a1064288f95c5ac5
Bruna Silva <brunacsilva11@gmail.com>
2021-06-19 09:32:32 -0300

    Confirmação de Alteração na Nova Marcacao

--- Proj.Gerenciamento_de_Salas/Pag_NovaMarcacao.Designer.cs
+++ Proj.Gerenciamento_de_Salas/Pag_NovaMarcacao.Designer.cs
@@ -30,26 +30,43 @@ namespace Proj.Gerenciamento_de_Salas
         private void InitializeComponent()
         {
             this.btn_Voltar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.SuspendLayout();
             // 
             // btn_Voltar
             // 
-            this.btn_Voltar.Location = new System.Drawing.Point(613, 36);
-            this.btn_Voltar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_Voltar.Location = new System.Drawing.Point(724, 12);
             this.btn_Voltar.Name = "btn_Voltar";
-            this.btn_Voltar.Size = new System.Drawing.Size(159, 42);
+            this.btn_Voltar.Size = new System.Drawing.Size(119, 34);
             this.btn_Voltar.TabIndex = 0;
             this.btn_Voltar.Text = "Voltar";
             this.btn_Voltar.UseVisualStyleBackColor = true;
             this.btn_Voltar.Click += new System.EventHandler(this.btn_Voltar_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(96, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(331, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(96, 100);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(331, 20);
+            this.dateTimePicker1.TabIndex = 2;
+            // 
             // Pag_NovaMarcacao
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(869, 490);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.btn_Voltar);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.Name = "Pag_NovaMarcacao";
             this.Text = "Pag_NovaMarcacao";
@@ -57,11 +74,14 @@ namespace Proj.Gerenciamento_de_Salas
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Pag_NovaMarcacao_FormClosing);
             this.Load += new System.EventHandler(this.Pag_NovaMarcacao_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btn_Voltar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
     }
 }
