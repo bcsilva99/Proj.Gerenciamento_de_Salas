87aabd6c34966c90564b7d2824dd1d2bc374c517
Carlos Glufke Chaves <1030150@sga.pucminas.br>
2021-06-19 16:01:33 -0300

    Editar Paciente (completo)

--- Proj.Gerenciamento_de_Salas/Pag_EditarPaciente.Designer.cs
+++ Proj.Gerenciamento_de_Salas/Pag_EditarPaciente.Designer.cs
@@ -36,14 +36,17 @@ namespace Proj.Gerenciamento_de_Salas
             this.txt_NomePaciente = new System.Windows.Forms.TextBox();
             this.txt_TelPaciente = new System.Windows.Forms.TextBox();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btn_Voltar
             // 
-            this.btn_Voltar.Location = new System.Drawing.Point(325, 98);
+            this.btn_Voltar.Location = new System.Drawing.Point(433, 121);
+            this.btn_Voltar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn_Voltar.Name = "btn_Voltar";
-            this.btn_Voltar.Size = new System.Drawing.Size(119, 34);
+            this.btn_Voltar.Size = new System.Drawing.Size(159, 42);
             this.btn_Voltar.TabIndex = 1;
             this.btn_Voltar.Text = "Voltar";
             this.btn_Voltar.UseVisualStyleBackColor = true;
@@ -52,69 +55,93 @@ namespace Proj.Gerenciamento_de_Salas
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(45, 15);
+            this.label1.Location = new System.Drawing.Point(60, 18);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(101, 13);
+            this.label1.Size = new System.Drawing.Size(132, 17);
             this.label1.TabIndex = 1;
             this.label1.Text = "Nome do Paciente :";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(31, 59);
+            this.label2.Location = new System.Drawing.Point(41, 53);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(115, 13);
+            this.label2.Size = new System.Drawing.Size(151, 17);
             this.label2.TabIndex = 2;
             this.label2.Text = "Telefone do Paciente :";
             // 
             // btn_Salvar
             // 
-            this.btn_Salvar.Location = new System.Drawing.Point(187, 98);
+            this.btn_Salvar.Location = new System.Drawing.Point(249, 121);
+            this.btn_Salvar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn_Salvar.Name = "btn_Salvar";
-            this.btn_Salvar.Size = new System.Drawing.Size(119, 34);
+            this.btn_Salvar.Size = new System.Drawing.Size(159, 42);
             this.btn_Salvar.TabIndex = 8;
             this.btn_Salvar.Text = "Salvar Edição";
             this.btn_Salvar.UseVisualStyleBackColor = true;
             // 
             // txt_NomePaciente
             // 
-            this.txt_NomePaciente.Location = new System.Drawing.Point(171, 12);
+            this.txt_NomePaciente.Location = new System.Drawing.Point(228, 15);
+            this.txt_NomePaciente.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txt_NomePaciente.Name = "txt_NomePaciente";
-            this.txt_NomePaciente.Size = new System.Drawing.Size(352, 20);
+            this.txt_NomePaciente.Size = new System.Drawing.Size(468, 22);
             this.txt_NomePaciente.TabIndex = 9;
             // 
             // txt_TelPaciente
             // 
-            this.txt_TelPaciente.Location = new System.Drawing.Point(171, 52);
+            this.txt_TelPaciente.Location = new System.Drawing.Point(228, 53);
+            this.txt_TelPaciente.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txt_TelPaciente.Name = "txt_TelPaciente";
-            this.txt_TelPaciente.Size = new System.Drawing.Size(352, 20);
+            this.txt_TelPaciente.Size = new System.Drawing.Size(468, 22);
             this.txt_TelPaciente.TabIndex = 10;
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.txt_TelPaciente);
             this.panel1.Controls.Add(this.btn_Voltar);
             this.panel1.Controls.Add(this.txt_NomePaciente);
             this.panel1.Controls.Add(this.btn_Salvar);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(27, 43);
+            this.panel1.Location = new System.Drawing.Point(36, 53);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(575, 157);
+            this.panel1.Size = new System.Drawing.Size(767, 193);
             this.panel1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(142, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 17);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Email :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(228, 87);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(468, 22);
+            this.textBox1.TabIndex = 12;
             // 
             // Pag_EditarPaciente
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(669, 259);
+            this.ClientSize = new System.Drawing.Size(889, 309);
             this.ControlBox = false;
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(685, 298);
+            this.MaximumSize = new System.Drawing.Size(907, 356);
             this.MinimizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(685, 298);
+            this.MinimumSize = new System.Drawing.Size(907, 356);
             this.Name = "Pag_EditarPaciente";
             this.ShowIcon = false;
             this.Text = "Pag_EditarPaciente";
@@ -134,5 +161,7 @@ namespace Proj.Gerenciamento_de_Salas
         private System.Windows.Forms.TextBox txt_NomePaciente;
         private System.Windows.Forms.TextBox txt_TelPaciente;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
     }
 }
--- Proj.Gerenciamento_de_Salas/Pag_NovaMarcacao.Designer.cs
+++ Proj.Gerenciamento_de_Salas/Pag_NovaMarcacao.Designer.cs
@@ -31,6 +31,8 @@ namespace Proj.Gerenciamento_de_Salas
         {
             this.btn_Voltar = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBoxSala = new System.Windows.Forms.ComboBox();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.txt_TelPaciente = new System.Windows.Forms.TextBox();
@@ -42,17 +44,15 @@ namespace Proj.Gerenciamento_de_Salas
             this.dateTimePrimeiraConsulta = new System.Windows.Forms.DateTimePicker();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.comboBoxSala = new System.Windows.Forms.ComboBox();
-            this.label6 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btn_Voltar
             // 
-            this.btn_Voltar.Location = new System.Drawing.Point(555, 296);
-            this.btn_Voltar.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_Voltar.Location = new System.Drawing.Point(740, 364);
+            this.btn_Voltar.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.btn_Voltar.Name = "btn_Voltar";
-            this.btn_Voltar.Size = new System.Drawing.Size(119, 34);
+            this.btn_Voltar.Size = new System.Drawing.Size(159, 42);
             this.btn_Voltar.TabIndex = 0;
             this.btn_Voltar.Text = "Voltar";
             this.btn_Voltar.UseVisualStyleBackColor = true;
@@ -74,48 +74,93 @@ namespace Proj.Gerenciamento_de_Salas
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.btn_Voltar);
-            this.panel1.Location = new System.Drawing.Point(59, 43);
+            this.panel1.Location = new System.Drawing.Point(79, 53);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(678, 344);
+            this.panel1.Size = new System.Drawing.Size(904, 423);
             this.panel1.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(149, 250);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 17);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Sala :";
+            // 
+            // comboBoxSala
+            // 
+            this.comboBoxSala.FormattingEnabled = true;
+            this.comboBoxSala.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "20",
+            "22",
+            "27"});
+            this.comboBoxSala.Location = new System.Drawing.Point(228, 246);
+            this.comboBoxSala.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBoxSala.Name = "comboBoxSala";
+            this.comboBoxSala.Size = new System.Drawing.Size(363, 24);
+            this.comboBoxSala.TabIndex = 13;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(473, 101);
+            this.label5.Location = new System.Drawing.Point(631, 124);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.Size = new System.Drawing.Size(63, 17);
             this.label5.TabIndex = 12;
             this.label5.Text = "Horário :";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(11, 98);
+            this.label4.Location = new System.Drawing.Point(15, 121);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(135, 13);
+            this.label4.Size = new System.Drawing.Size(181, 17);
             this.label4.TabIndex = 11;
             this.label4.Text = "Data da Primeira Consulta :";
             // 
             // txt_TelPaciente
             // 
-            this.txt_TelPaciente.Location = new System.Drawing.Point(171, 52);
+            this.txt_TelPaciente.Location = new System.Drawing.Point(228, 64);
+            this.txt_TelPaciente.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txt_TelPaciente.Name = "txt_TelPaciente";
-            this.txt_TelPaciente.Size = new System.Drawing.Size(273, 20);
+            this.txt_TelPaciente.Size = new System.Drawing.Size(363, 22);
             this.txt_TelPaciente.TabIndex = 10;
             // 
             // txt_NomePaciente
             // 
-            this.txt_NomePaciente.Location = new System.Drawing.Point(171, 12);
+            this.txt_NomePaciente.Location = new System.Drawing.Point(228, 15);
+            this.txt_NomePaciente.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txt_NomePaciente.Name = "txt_NomePaciente";
-            this.txt_NomePaciente.Size = new System.Drawing.Size(273, 20);
+            this.txt_NomePaciente.Size = new System.Drawing.Size(363, 22);
             this.txt_NomePaciente.TabIndex = 9;
             // 
             // btn_Salvar
             // 
-            this.btn_Salvar.Location = new System.Drawing.Point(414, 296);
+            this.btn_Salvar.Location = new System.Drawing.Point(552, 364);
+            this.btn_Salvar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn_Salvar.Name = "btn_Salvar";
-            this.btn_Salvar.Size = new System.Drawing.Size(119, 34);
+            this.btn_Salvar.Size = new System.Drawing.Size(159, 42);
             this.btn_Salvar.TabIndex = 8;
             this.btn_Salvar.Text = "Salvar Marcação";
             this.btn_Salvar.UseVisualStyleBackColor = true;
@@ -130,17 +175,19 @@ namespace Proj.Gerenciamento_de_Salas
             "Carlos",
             "Gabriela",
             "Thyago"});
-            this.comboBoxAluno.Location = new System.Drawing.Point(171, 150);
+            this.comboBoxAluno.Location = new System.Drawing.Point(228, 185);
+            this.comboBoxAluno.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBoxAluno.Name = "comboBoxAluno";
-            this.comboBoxAluno.Size = new System.Drawing.Size(273, 21);
+            this.comboBoxAluno.Size = new System.Drawing.Size(363, 24);
             this.comboBoxAluno.TabIndex = 7;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(106, 150);
+            this.label3.Location = new System.Drawing.Point(141, 185);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.Size = new System.Drawing.Size(52, 17);
             this.label3.TabIndex = 6;
             this.label3.Text = "Aluno :";
             // 
@@ -161,72 +208,52 @@ namespace Proj.Gerenciamento_de_Salas
             "18:00",
             "19:00",
             "20:00"});
-            this.comboBoxHorario.Location = new System.Drawing.Point(526, 98);
+            this.comboBoxHorario.Location = new System.Drawing.Point(701, 121);
+            this.comboBoxHorario.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBoxHorario.Name = "comboBoxHorario";
-            this.comboBoxHorario.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxHorario.Size = new System.Drawing.Size(160, 24);
             this.comboBoxHorario.TabIndex = 5;
             // 
             // dateTimePrimeiraConsulta
             // 
-            this.dateTimePrimeiraConsulta.Location = new System.Drawing.Point(171, 98);
+            this.dateTimePrimeiraConsulta.Location = new System.Drawing.Point(228, 121);
+            this.dateTimePrimeiraConsulta.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dateTimePrimeiraConsulta.Name = "dateTimePrimeiraConsulta";
-            this.dateTimePrimeiraConsulta.Size = new System.Drawing.Size(273, 20);
+            this.dateTimePrimeiraConsulta.Size = new System.Drawing.Size(363, 22);
             this.dateTimePrimeiraConsulta.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(31, 59);
+            this.label2.Location = new System.Drawing.Point(41, 73);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(115, 13);
+            this.label2.Size = new System.Drawing.Size(151, 17);
             this.label2.TabIndex = 2;
             this.label2.Text = "Telefone do Paciente :";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(45, 15);
+            this.label1.Location = new System.Drawing.Point(60, 18);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(101, 13);
+            this.label1.Size = new System.Drawing.Size(132, 17);
             this.label1.TabIndex = 1;
             this.label1.Text = "Nome do Paciente :";
             // 
-            // comboBoxSala
-            // 
-            this.comboBoxSala.FormattingEnabled = true;
-            this.comboBoxSala.Items.AddRange(new object[] {
-            "Arthur",
-            "Bruna",
-            "Bruno",
-            "Carlos",
-            "Gabriela",
-            "Thyago"});
-            this.comboBoxSala.Location = new System.Drawing.Point(171, 200);
-            this.comboBoxSala.Name = "comboBoxSala";
-            this.comboBoxSala.Size = new System.Drawing.Size(273, 21);
-            this.comboBoxSala.TabIndex = 13;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(112, 203);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(34, 13);
-            this.label6.TabIndex = 14;
-            this.label6.Text = "Sala :";
-            // 
             // Pag_NovaMarcacao
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1064, 544);
             this.ControlBox = false;
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(816, 489);
+            this.MaximumSize = new System.Drawing.Size(1082, 591);
             this.MinimizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(816, 489);
+            this.MinimumSize = new System.Drawing.Size(1082, 591);
             this.Name = "Pag_NovaMarcacao";
             this.ShowIcon = false;
             this.Text = "Pag_NovaMarcacao";
